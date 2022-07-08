966e7fc5fa222efb79db9b50b48c390858189408
Xudoyberdi <xudoyberdi3003@gmail.com>
2022-07-08 11:35:49 +0500

    addUniversityDto

--- src/main/java/uz/pdp/appjparelationships/payload/UniversityDto.java
+++ src/main/java/uz/pdp/appjparelationships/payload/UniversityDto.java
@@ -4,7 +4,8 @@ package uz.pdp.appjparelationships.payload;
 import lombok.Data;
 
 @Data
-public class UniversityDto {//MA'LUMOTLARNI TASHISH UCHUN XIZMAT QILADI
+public class UniversityDto {
+    //MA'LUMOTLARNI TASHISH UCHUN XIZMAT QILADI
     private String name;
     private String city;
     private String district;
